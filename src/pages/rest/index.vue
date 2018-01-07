25ddfd05b1b845c7727f14c66d690350457b4506
guominyi <1621575585@qq.com>
2018-01-07 18:48:24 +0800

    rest-update

--- src/pages/rest/index.vue
+++ src/pages/rest/index.vue
@@ -1,22 +1,52 @@
 <template>
   <div class="index">
     <rest-header></rest-header>
+    <rest-video :videoList="videoList" v-show="false"></rest-video>
+    <rest-recreation :reaVideo="reaVideo" :reaList="reaList"></rest-recreation>
   </div>
 </template>
 
 <script>
   import RestHeader from 'components/common/header.vue'
+  import RestRecreation from './recreation.vue'
+  import RestVideo from './video.vue'
+  import axios from 'axios'
   export default {
     name: 'rest',
+    data () {
+      return {
+        videoList: [],
+        reaVideo: [],
+        reaList: []
+      }
+    },
     components: {
-      RestHeader
+      RestHeader,
+      RestVideo,
+      RestRecreation
+    },
+    mounted () {
+      axios.get('/api/rest.json')
+           .then(this.handleSuccessRest.bind(this))
+           .catch(this.handleErrorRest.bind(this))
+    },
+    methods: {
+      handleSuccessRest (res) {
+        console.log(res)
+        let ret = (res.data) ? (res = res.data) : res
+        ret && (this.videoList = ret.data.video)
+        ret && (this.reaVideo = ret.data.reaVideo)
+        ret && (this.reaList = ret.data.reaList)
+      },
+      handleErrorRest (err) {
+        console.log(err)
+      }
     }
   }
 </script>
 
 <style scoped>
-  .index{
-    position: absolute;
+  .index {
     width: 100%;
     height: 100%;
     background: #3fe9a0;
---
+++ src/pages/rest/recreation.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="index-recreation">
+     <div class="content" v-for="item in reaVideo" :key="item.id">
+      <video :src="item.videoUrl" class="video" controls coop :poster="item.imgUrl"></video>
+      <div class="desc">
+        <p>{{item.style}}</p>
+        <p>{{item.author}}</p>
+        <p>评论{{item.comment}}</p>
+        <p>{{item.time}}</p>
+      </div>
+    </div>
+
+
+    <div class="list" v-for="item in reaList" :key="item.id"> 
+      <p class="title">{{item.title}}</p>
+      <div class="img-box">
+        <div class="img-cont" v-for="items in item.imgBox" :key="items.imgUrl">
+          <img :src="items.imgUrl" alt="">
+        </div>
+      </div>
+      <div class="desc">
+        <p>{{item.style}}</p>
+        <p>{{item.author}}</p>
+        <p>评论{{item.comment}}</p>
+        <p>{{item.time}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'recreation',
+    props: {
+      reaList: Array,
+      reaVideo: Array
+    }
+  }
+</script>
+
+<style scoped>
+  .index-recreation {
+    margin: .2rem;
+    background: #fff;
+  }
+  .content {
+    padding: .2rem .3rem;
+    border-bottom: 1px solid #000;
+  }
+  .video {
+    width: 100%;
+  }
+  .desc {
+    display: flex;
+    font-size: .12rem;
+    color: #000;
+  }
+  .desc p {
+    line-height: .5rem;
+    margin-left: .3rem;
+  }
+  .list {
+    border-bottom: 1px solid #000;
+  }
+  .title {
+    line-height: .7rem;
+    font-size: .3rem;
+    color: #000;
+    padding: 0 .2rem;
+  }
+  .img-box {
+    display: flex;
+    margin-bottom: .1rem;
+  }
+  .img-cont {
+    width: 33.33%;
+    padding: 0 0.1rem;
+  }
+  .img-cont img {
+    width: 100%;
+  }
+</style>
