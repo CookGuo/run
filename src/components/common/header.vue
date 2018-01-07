3ec4f78ee63531f37d236308d148037dd1026f3c
czy <czy521962464@162.com>
2018-01-07 15:38:09 +0800

    123

---
+++ src/components/common/header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header">
+    <div class="header-top">
+      <p class="top-con">今日<span class="top-num">5.0</span>公里</p>
+      <div class="top-start">开始跑步</div>
+    </div>
+    <div class="header-nav">
+      <div class="nav-item nav-active">推荐</div>
+      <div class="nav-item">推荐</div>
+      <div class="nav-item">推荐</div>
+      <div class="nav-item">推荐</div>
+      <div class="nav-item">推荐</div>
+    </div>
+    <input class="header-input" placeholder="请输入搜索内容"></input>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Restheader'
+  }
+</script>
+
+<style scoped>
+  .header{
+    padding: .1rem 0.2rem 0;
+  }
+  .header-top{
+    width: 100%;
+    height: 1rem;
+    border-radius: .08rem;
+    background: #fff;
+    position: relative;
+  }
+  .top-con{
+    font-size: .28rem;
+    color: #333;
+    line-height: 1rem;
+    margin-left: .2rem;
+  }
+  .top-num{
+    color: #3fe9a0;
+  }
+  .top-start{
+    position: absolute;
+    width: 1.17rem;
+    right: 0.2rem;
+    top:.23rem;
+    line-height: .44rem;
+    background: #3fe9a0;
+    font-size: .2rem;
+    text-align: center;
+    color: #fff;
+    border-radius: .2rem;
+  }
+  .header-nav{
+    display: flex;
+    padding: 0 .2rem;
+    justify-content: space-between;
+  }
+  .nav-item{
+    line-height: .88rem;
+    font-size: .32rem;
+  }
+  .nav-active{
+    color: #fff;
+  }
+  .header-input{
+    height: .6rem;
+    width: 100%;
+    border-radius: .28rem;
+    text-align: center;
+    font-size: .28rem;
+    color: #9a9a9a;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
+import Rest from '@/pages/rest/index'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/rest',
+      name: 'Rest',
+      component: Rest
     }
   ]
 })
