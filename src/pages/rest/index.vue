5000bfece87367266ce7e64be2cbd09bca56a59f
czy <czy521962464@162.com>
2018-01-07 18:52:44 +0800

    11

--- src/pages/rest/index.vue
+++ src/pages/rest/index.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="index">
     <rest-header></rest-header>
+    <recommend :recommends='recommends'></recommend>
+    <rest-bottom></rest-bottom>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import RestHeader from 'components/common/header.vue'
+  import RestBottom from 'components/common/bottom.vue'
+  import Recommend from './recommend.vue'
   export default {
     name: 'rest',
     components: {
-      RestHeader
+      RestHeader,
+      Recommend,
+      RestBottom
+    },
+    data () {
+      return {
+        recommends: []
+      }
+    },
+    mounted () {
+      axios.get('api/rest.json')
+           .then(this.handleRestAjaxSucc.bind(this))
+           .catch(this.handleRestAjaxErr.bind(this))
+    },
+    methods: {
+      handleRestAjaxSucc (res) {
+        res = (res.data) ? res.data : null
+        if (res) {
+          this.recommends = res.data.recommends
+        }
+      },
+      handleRestAjaxErr (err) {
+        console.log(err)
+      }
     }
   }
 </script>
