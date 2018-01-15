9ea58ae29b91b913793b7b63d6d9af1b79add05c
czy <czy521962464@162.com>
2018-01-15 10:49:28 +0800

    432

--- src/pages/about/container.vue
+++ src/pages/about/container.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="container">
-    
+  <div class="container border-top">
+    <div class="about">
+    	<div>用户名:<span>XXX</span></div>
+    	<div>账&nbsp;&nbsp;&nbsp;&nbsp;号:<span>101129101</span></div>
+    </div>
+    <div class="con-list">
+    	<div class="con-item">我的成就</div>
+    	<div class="con-item">关注</div>
+    	<div class="con-item">通知</div>
+    	<div class="con-item">主页</div>
+    </div>
+    <div class="cancellation border-topbottom">注销登录</div>
   </div>
 </template>
 
@@ -13,7 +23,28 @@
 <style scoped>
   .container {
     flex: 1;
-    background: green;
+    background: #fff;
   }
-  
+  .about{
+  	height: .8rem;
+  	background: #42c69a;
+  	padding: .4rem;
+  	display: flex;
+  	justify-content: space-between;
+  	flex-direction: column;
+  	font-size: .28rem;
+  	color: #fff;
+  }
+  .con-list{
+  	display: flex;
+  	justify-content: space-around;
+  	line-height: 1rem;
+  }
+  .cancellation{
+  	height: .8rem;
+  	color: red;
+  	line-height: .8rem;
+  	text-align: center;
+  	font-size: .36rem;
+  }
 </style>
